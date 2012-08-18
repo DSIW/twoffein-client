d6ddb7794328c5196760785380910c742b86e270
DSIW <dsiw@dsiw-it.de>
2012-08-18 14:06:16 +0200

    Add feature to post cookies

--- lib/twoffein-client.rb
+++ lib/twoffein-client.rb
@@ -215,4 +215,23 @@ module Twoffein
       s << "mit #{@with_screen_name}" if @with_screen_name
     end
   end
+
+  class Cookie
+    attr_accessor :target_screen_name
+
+    def initialize target_screen_name=nil
+      @target_screen_name = target_screen_name
+    end
+
+    def post
+      info =  HTTP.post("cookie", :target_screen_name => @target_screen_name)
+      raise Error.new(info[:code], info[:error]) if info.has_key? :error
+      info
+    end
+    alias send post
+
+    def to_s
+      "Ich gebe #@target_screen_name einen Keks!"
+    end
+  end
 end
