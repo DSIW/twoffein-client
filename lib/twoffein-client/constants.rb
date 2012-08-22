7e0e84c99a27649e5728e605640b3d1b525c695d
DSIW <dsiw@dsiw-it.de>
2012-08-22 16:54:03 +0200

    Twoffein url moved to twoffein.com

--- lib/twoffein-client/constants.rb
+++ lib/twoffein-client/constants.rb
@@ -1,7 +1,7 @@
 module Twoffein
   SCREEN_NAME = ENV['TWOFFEIN_SCREEN_NAME']
   API_KEY     = ENV['TWOFFEIN_API_KEY']
-  BASE_URL    = 'http://twoffein.de/api/'
+  BASE_URL    = 'http://twoffein.com/api/'
   PARAMS = {
     :screen_name => SCREEN_NAME,
     :api_key => API_KEY
