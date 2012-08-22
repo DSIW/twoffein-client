890cce6faed8fc264440f0e6161da671871af19e
DSIW <dsiw@dsiw-it.de>
2012-08-22 15:59:15 +0200

    Add Error in module Server and add a to_s

--- lib/twoffein-client/exceptions.rb
+++ lib/twoffein-client/exceptions.rb
@@ -1,9 +1,15 @@
 module Twoffein
-  class Error < StandardError
-    attr_reader :type, :message
-    def initialize(type, message)
-      @type = type
-      @message = message
+  module Server
+    class Error < StandardError
+      attr_reader :type, :message
+      def initialize(type, message)
+        @type = type
+        @message = message
+      end
+
+      def to_s
+        "Server::Error: #@type (#@message)"
+      end
     end
   end
 end
