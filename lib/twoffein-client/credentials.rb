0f8e2df1614c15d7a60b966df87a3d10dd2badb0
DSIW <dsiw@dsiw-it.de>
2012-08-23 19:36:12 +0200

    Fix credentials

--- lib/twoffein-client/credentials.rb
+++ lib/twoffein-client/credentials.rb
@@ -1,8 +1,5 @@
-#!/usr/bin/env ruby
-# encoding: utf-8
-#
 # You have to set your user name and your API key
 # Please visit http://twoffein.de/users/<USERNAME>/#api to get your API key.
 
-ENV['TWOFFEIN_SCREEN_NAME'] = "USER_NAME"
-ENV['TWOFFEIN_API_KEY']     = "KEY"
+#ENV['TWOFFEIN_SCREEN_NAME'] = "USER_NAME"
+#ENV['TWOFFEIN_API_KEY']     = "KEY"
